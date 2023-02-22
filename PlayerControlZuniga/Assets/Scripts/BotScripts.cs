bf1514afb0d15e02fc55213ae1bc5e39b92efea7
MattZ101 <754255@student.dusd.net>
2023-02-22 09:55:10 -0800

    more cars
    i added more cars on my scripts for i could dodge them. and to turn my car

---
+++ PlayerControlZuniga/Assets/Scripts/BotScripts.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotScripts : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float fowardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        fowardInput = Input.GetAxis("Vertical");
+        // Move the car foward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
